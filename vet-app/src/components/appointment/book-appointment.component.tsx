6b6fe40a7792d74bbd9a93602510ceba4748afd3
Ayeah Chanser <ayeahchanser@gmail.com>
2024-03-18 13:23:31 +0100

    appointment page

--- vet-app/src/components/appointment/book-appointment.component.tsx
+++ vet-app/src/components/appointment/book-appointment.component.tsx
@@ -1,23 +1,77 @@
 // AppointmentForm.tsx
 
 import React, { useState } from "react";
-import { Form, Input, DatePicker, Button, message } from "antd";
+import { Form, Input, DatePicker, Button, message, Select } from "antd";
 import { Moment } from "moment";
+import { useUserSpecialty } from "hooks/user-specialty.hook";
 
 const AppointmentForm: React.FC = () => {
+  const { userSpecialties } = useUserSpecialty();
   const handleSubmit = (values: any) => {
     console.log("values: ", values);
   };
 
+  const onSearch = (value: string) => {
+    console.log("search:", value);
+  };
+
+  // Filter `option.label` match the user type `input`
+  const filterOption = (
+    input: string,
+    option?: { label: string; value: string }
+  ) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
+
   return (
     <Form layout="vertical" initialValues={{}} onFinish={handleSubmit}>
       <Form.Item
-        label="Name"
+        label="Full Name"
         name={"name"}
         rules={[{ required: true, message: "Your Name is Required!" }]}
       >
         <Input />
       </Form.Item>
+      <Form.Item
+        label="Email"
+        name={"email"}
+        rules={[{ required: true, message: "Your Email is Required!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Contact Number"
+        name={"contactNo"}
+        rules={[{ required: true, message: "Your Contact is Required!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Doctor's Name"
+        name={"doctor"}
+        rules={[
+          {
+            required: true,
+            message: "Select a medical practitioner to consult",
+          },
+        ]}
+      >
+        <Select
+          showSearch
+          placeholder="Select Runner Period"
+          optionFilterProp="children"
+          onChange={onSearch}
+          onSearch={onSearch}
+          filterOption={filterOption}
+          options={userSpecialties.map((r) => {
+            return {
+              label: `${r.username}`,
+              value: r.userId,
+            };
+          })}
+        />
+      </Form.Item>
+
       <Form.Item
         label="Appointment Date"
         name={"appointmentDate"}
@@ -25,6 +79,23 @@ const AppointmentForm: React.FC = () => {
       >
         <DatePicker style={{ width: "100%" }} />
       </Form.Item>
+
+      <Form.Item
+        label="Appointment Time"
+        name={"appointmentTime"}
+        rules={[{ required: true, message: "Time is Required!" }]}
+      >
+        <DatePicker style={{ width: "100%" }} />
+      </Form.Item>
+
+      <Form.Item
+        label="Symptoms"
+        name={"symptoms"}
+        rules={[{ required: true, message: "Enter identifiable symptoms!" }]}
+      >
+        <Input.TextArea rows={3} />
+      </Form.Item>
+
       <Form.Item>
         <Button type="primary" onClick={handleSubmit}>
           Book Appointment
--- vet-app/src/pages/service/book.page.tsx
+++ vet-app/src/pages/service/book.page.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "antd";
+import { Card, Col, Row, Typography } from "antd";
 import AppointmentForm from "components/appointment/book-appointment.component";
 import React from "react";
 import { Helmet } from "react-helmet-async";
@@ -15,8 +15,26 @@ const BookAppointmentsPage = () => {
           content="Browse through a carefully curated collection of vet-approved products at Linkavet. Elevate your pet's lifestyle with our premium range of nutrition, grooming essentials, toys, and wellness products. Each item is selected with your pet's health and happiness in mind. Shop confidently for top-quality products that complement our commitment to excellence in veterinary care. Enhance your pet's life today with Linkavet."
         />
       </Helmet>
-      <Typography.Title level={2}>Book Appointment</Typography.Title>
-      <AppointmentForm />
+      <Row gutter={[16, 16]} justify={"center"} align={"top"} style={{ margin: "3rem auto"}}>
+        <Col xs={22} md={10}>
+          <Card bordered={false}>
+            <Typography.Title level={2}>Book Appointment</Typography.Title>
+            <AppointmentForm />
+          </Card>
+        </Col>
+        <Col xs={22} md={12}>
+          <Card
+            bordered={false}
+            style={{ borderRadius: 0 }}
+            cover={
+              <img
+                src="/images/daniel-leone-LXQx98FPPQ4-unsplash.jpg"
+                alt="a herd of cows"
+              />
+            }
+          />
+        </Col>
+      </Row>
     </>
   );
 };
